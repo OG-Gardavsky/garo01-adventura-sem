b510949ac6fdb602ff2b4c1576df6b7343a1ea6a
unknown <ogardavsky@gmail.com>
2019-11-05 19:51:31 +0100

    Basic FXML and Controller setup

--- src/main/java/cz/vse/java/garo01/logika/Controller.java
+++ src/main/java/cz/vse/java/garo01/logika/Controller.java
@@ -3,6 +3,7 @@ package cz.vse.java.garo01.logika;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,6 +11,9 @@ import java.io.FileNotFoundException;
 public class Controller {
 
     @FXML
+    private VBox seznamJit;
+    @FXML
+    private VBox seznamPlizit;
     private IHra hra;
     @FXML
     private ImageView obrazekLokace;

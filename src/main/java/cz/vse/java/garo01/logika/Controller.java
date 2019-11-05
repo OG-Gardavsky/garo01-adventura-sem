00ed4ad9282c9a0079912ce8d1dcb331701d9421
unknown <ogardavsky@gmail.com>
2019-11-05 17:52:17 +0100

    added image for testing background

--- src/main/java/cz/vse/java/garo01/logika/Controller.java
+++ src/main/java/cz/vse/java/garo01/logika/Controller.java
@@ -1,16 +1,29 @@
 package cz.vse.java.garo01.logika;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Controller {
 
     @FXML
     private IHra hra;
+    @FXML
+    private ImageView obrazekLokace;
 
     public void setHra(IHra hra){
         this.hra = hra;
         HerniPlan herniPlan = hra.getHerniPlan();
         Prostor aktualniProstor = herniPlan.getAktualniProstor();
+        zmenProstor(aktualniProstor);
+    }
+
+    private void zmenProstor(Prostor prostor){
+        Image image = new Image(getClass().getResourceAsStream("/" + prostor.getNazev() + ".jpg"));
+        obrazekLokace.setImage(image);
 
     }
 
